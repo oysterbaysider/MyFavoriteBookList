958eec77d428f33dc2ab745e8b12fe201d575fd3
Ting Li <oysterbaysider@gmail.com>
2018-10-26 16:39:30 +1100

    Project Initiated

---
+++ UsersApi/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsersApi.Models
+{
+    public class User
+    {
+        [BsonId]
+        public ObjectId UserID { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserDisplayName { get; set; }
+
+        public int UserAge { get; set; }
+    }
+}
---
+++ UsersApi/Repositories/IUsersRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsersApi.Models;
+
+namespace UsersApi.Repositories
+{
+    interface IUsersRepository
+    {
+        Task CreateAsync(User user);
+
+        Task<IEnumerable<User>> GetAllAds();
+
+        Task<User> GetGuitarAD(string userId);
+
+        Task<bool> Update(User user);
+        Task<bool> Delete(string userId);
+    }
+}
